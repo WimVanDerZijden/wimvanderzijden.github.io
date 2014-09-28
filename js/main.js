2011cf56c10de779583d96aa4959b8c6ebc0c09f
Wim van der Zijden <w.vanderzijden@hotmail.nl>
2014-09-28 19:41:19 +0200

    ie9 compatibility

--- js/main.js
+++ js/main.js
@@ -1,54 +1,64 @@
 "use strict";
-// Add class 'show-mobile' to display the menu when in mobile mode, when
-// menu-button was clicked.
-var toggleMobileMenu = function () {
-  document.getElementById("menu").classList.toggle('show');
-};
 
-document.getElementById('menu-button').addEventListener('click', toggleMobileMenu, false);
-
-// Show the login dialog when clicking login
-var showLoginDialog = function () {
-  document.getElementById('login').style.display = 'block';
-  document.getElementById('dark').style.display = 'block';
-};
-
-document.getElementById('login-link').addEventListener('click', showLoginDialog, false);
-
-// Hide the login dialog when cliking on the area around it (#dark)
-var hideLoginDialog = function () {
-  document.getElementById('login').style.display = 'none';
-  document.getElementById('dark').style.display = 'none';
-};
-
-document.getElementById('dark').addEventListener('click', hideLoginDialog, false);
-
-window.onkeyup = function (e) {
-  var key = e.keyCode ? e.keyCode : e.which;
-  if (key === 27) {
-    hideLoginDialog();
-  }
-};
-
-/* Playfully rotate the article images */
-
-var articles = document.getElementsByTagName('article');
-// The higher the dullness, the smaller the rotations.
-// The maximum rotation angle is PI over dullness over 2 radians.
-var DULLNESS = 12;
-var n, i;
-for (n = 0; n < articles.length; n += 1) {
-  var images = articles[n].getElementsByTagName('img');
-  for (i = 0; i < images.length; i += 1) {
-    var rad = Math.random() * Math.PI / DULLNESS - Math.PI / DULLNESS / 2;
-    images[i].style.transform = 'rotate(' + rad + 'rad)';
-    // For debugging: maximum rotation
-    //images[i].style.transform = 'rotate(' + (-Math.PI / DULLNESS / 2) + 'rad)';
-  }
+// IE 8 crashes on some of the javascript.
+// Because we decided not to support IE 8, we simply disable it all.
+// We also hide all site content.
+if (document.getElementById('ie8') != null) {
+	document.getElementById("wrapper").style.display = 'none';
 }
-
-/* Pretend we've just submitted the contact form with AJAX */
-var submitForm = function () {
-  document.getElementById("form-fill-in").style.display = 'none';
-  document.getElementById("form-sent").style.display = 'block';
-};
+else {
+	
+	// Add class 'show-mobile' to display the menu when in mobile mode, when
+	// menu-button was clicked.
+	var toggleMobileMenu = function () {
+	  document.getElementById("menu").classList.toggle('show');
+	};
+	
+	document.getElementById('menu-button').addEventListener('click', toggleMobileMenu, false);
+	
+	// Show the login dialog when clicking login
+	var showLoginDialog = function () {
+	  document.getElementById('login').style.display = 'block';
+	  document.getElementById('dark').style.display = 'block';
+	};
+	
+	document.getElementById('login-link').addEventListener('click', showLoginDialog, false);
+	
+	// Hide the login dialog when cliking on the area around it (#dark)
+	var hideLoginDialog = function () {
+	  document.getElementById('login').style.display = 'none';
+	  document.getElementById('dark').style.display = 'none';
+	};
+	
+	document.getElementById('dark').addEventListener('click', hideLoginDialog, false);
+	
+	window.onkeyup = function (e) {
+	  var key = e.keyCode ? e.keyCode : e.which;
+	  if (key === 27) {
+	    hideLoginDialog();
+	  }
+	};
+	
+	/* Playfully rotate the article images */
+	
+	var articles = document.getElementsByTagName('article');
+	// The higher the dullness, the smaller the rotations.
+	// The maximum rotation angle is PI over dullness over 2 radians.
+	var DULLNESS = 12;
+	var n, i;
+	for (n = 0; n < articles.length; n += 1) {
+	  var images = articles[n].getElementsByTagName('img');
+	  for (i = 0; i < images.length; i += 1) {
+	    var rad = Math.random() * Math.PI / DULLNESS - Math.PI / DULLNESS / 2;
+	    images[i].style.transform = 'rotate(' + rad + 'rad)';
+	    // For debugging: maximum rotation
+	    //images[i].style.transform = 'rotate(' + (-Math.PI / DULLNESS / 2) + 'rad)';
+	  }
+	}
+	
+	/* Pretend we've just submitted the contact form with AJAX */
+	var submitForm = function () {
+	  document.getElementById("form-fill-in").style.display = 'none';
+	  document.getElementById("form-sent").style.display = 'block';
+	};
+}
